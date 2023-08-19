c1cc14a6ab0e62176f08c95362c1b0e6bfdcf519
Software Shinobi <troy@softwareshinobi.online>
2023-08-19 13:48:39 -0500

    automated terminal push

--- repository-build-automation/docker-image-build-scripts/build-pass-1-build-docker-image-from-dockerfile.sh
+++ repository-build-automation/docker-image-build-scripts/build-pass-1-build-docker-image-from-dockerfile.sh
@@ -12,4 +12,14 @@ source ../custom-build-configuration/docker-image-repository-configuration.conf
 
 ##
 
+ls -lha ../../repository-source-code/
+
+rm      ../../repository-source-code/operating-system-provision-scripts
+
+ln -s   ../operating-system-provision-scripts/ ../../repository-source-code/
+
+ls -lha ../../repository-source-code/
+
+##
+
 docker build -f ../custom-build-configuration/Dockerfile -t $repositoryUser/$repositoryName:$repositoryTag ../../repository-source-code
